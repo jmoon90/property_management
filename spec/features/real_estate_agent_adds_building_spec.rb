59e4a576093c90ced48febbb6ccf00606f24f913
John Moon <johnmoonyy@gmail.com>
2014-01-04 14:01:01 -0500

    Wrote acceptance test to begin the outside in development

---
+++ spec/features/real_estate_agent_adds_building_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature "Real estate agent adds building" do
+  scenario 'with invalid input' do
+    new_building_path
+
+    click_on "submit"
+
+    expect(page).to have_content("Invalid information. Please try again")
+  end
+
+  scenario 'with valid input' do
+    new_building_path
+
+    fill_in :street_address, with: "33 harrisona ave"
+    fill_in :city, with: "Boston"
+    fill_in :state, with: "MA"
+    fill_in :postal_code, with: 39485
+    click_on "submit"
+
+    expect(page).to have_content("Successfully added building information")
+  end
+end
+
